06769bc18b204bff77f7af0a4efc003b46dbe7f5
Uliana Semyonova <i@ulianasemyonova.ru>
2023-11-21 21:55:22 +0500

    Fix error

--- main/management/commands/create_db.py
+++ main/management/commands/create_db.py
@@ -10,47 +10,6 @@ class Command(BaseCommand):
     FORMS = [("Form email, phone", [1, 2]), ("Form email, phone, text", [1, 2, 3]), ("Email", [4]), ("Date", [5]),
              ("Form email, date", [1,  6]), ("Form email, phone, text, name", [1, 2, 3, 7])]
 
-    db_data = [
-        {
-            "name": "Form email, phone",
-            "field_name_1": "email",
-            "field_name_2": "phone"
-        },
-        {
-            "name": "Form email, phone, text",
-            "field_name_1": "email",
-            "field_name_2": "phone",
-            "field_name_3": "text"
-        },
-        {
-            "name": "Email",
-            "email": "email"
-        },
-        {
-            "name": "Date",
-            "field_name_1": "date"
-        },
-        {
-            "name": "Form email, date",
-            "field_name_1": "email",
-            "field_name_2": "date"
-        },
-        {
-            "name": "Form email, phone, text, name",
-            "field_name_1": "email",
-            "field_name_2": "phone",
-            "field_name_3": "text",
-            "field_name_4": "date"
-        }
-    ]
-
-    #
-    # def handle(self, *args, **options):
-    #     db = TinyDB('templates.json')
-    #
-    #     for data in self.db_data:
-    #         db.insert(data)
-
     def handle(self, *args, **options):
 
         for type_ in ["email", "phone", "text", "date"]:
