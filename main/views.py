8a616ddc0d5a63168ba8ade4198aca3cc8e255eb
Uliana Semyonova <i@ulianasemyonova.ru>
2023-11-21 21:52:14 +0500

    Create view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from main.servises import check_format, search_form
+
+
+class FormAPIView(APIView):
+
+    def post(self, request):
+        query_params = request.query_params
+        validated_data = check_format(query_params)
+        response = search_form(validated_data)
+
+        if response:
+            return Response(status=status.HTTP_200_OK, data={"template_name": response.name})
+
+        return Response(status=status.HTTP_200_OK, data=validated_data)
